14350cca8b41be9eb0f3c6a432a85a86ceb6810a
jvlcek23 <kubevlcek@gmail.com>
2021-11-23 00:11:34 +0100

    Change color with disable click after first click

--- scripts.js
+++ scripts.js
@@ -5,23 +5,33 @@ $('#first').on('click',() => {
 $('#second').click(() => {
     document.getElementById('first').innerHTML = "You totally clicked the second button";
 });
-
-let btn = document.createElement("BUTTON");
-btn.setAttribute('id','third');
-btn.setAttribute('class','btns');
-btn.innerHTML = "Third Button";
-document.body.append(btn);
+let bttn = $("<button></button>").text("Third Button");
+bttn.attr('id', 'third');
+bttn.attr('class', 'btns');
+$('.buttons').append(bttn);
 
 
 $('#third').click(() => {
-$(".btns").css("backgroundColor","olive");
+var changecolor= $("#clr").val();
+$(".btns").css("backgroundColor",changecolor);
+$('#third').off('click');
 });
 
 
 
-//CREATE THIRD BUTTON
+
+
+//NOT WORKING THIRD BUTTON
 
 /* $(document).ready(function() {
-    $('body').append('<button class="btns" id="third" type="button">Third Button</button>');
-}); */
+    $('.buttons').append(<button id="third" class="btns" type="button">Third Button</button>);
+});  */
 
+//CREATE THIRD BUTTON
+
+/* let btn = document.createElement("BUTTON");
+btn.setAttribute('id','third');
+btn.setAttribute('class','btns');
+btn.innerHTML = "Third Button";
+document.body.append(btn);
+ */
